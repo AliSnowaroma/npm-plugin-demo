23e1e6086d5818c9fe6d2b5871bd45180a625892
bluetalk <772905605@qq.com>
2018-10-16 23:32:47 +0800

    create

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export default {
+  name:'测试',
+  test(){
+  	console.log('测试组件发布')
+  }
+}
---
+++ webpack.cconfig.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');//如果不想让它把某个文件打包进去，那么可以使用externals来进行过滤
+
+module.exports = {
+	entry:'./src/index.js',
+	output:{
+		filename:'bundle.js',
+		path:path.resolve(__dirname,'dist'),
+		libraryTarget:'common2.js',
+	},
+	modules:{
+		rules:[
+		  {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+		]
+	},
+	externals: [nodeExternals()],//// in order to ignore all modules in node_modules folder
+	plugins:[]//这里仅仅是把js打包，不需要把js放入html模板，所以不加HtmlWebpackPlugin
+}
